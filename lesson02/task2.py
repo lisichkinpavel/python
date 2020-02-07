64b84bdd320e41ba83e8bd6546df3bffe55bd67b
Pavel Lisichkin <pavel.lisichkin@gmail.com>
2020-02-08 02:44:44 +0300

    добавил решение с zip

--- lesson02/task2.py
+++ lesson02/task2.py
@@ -2,10 +2,16 @@
 input_data = '1 2 3 4 5 6 7 8 9 10 11'
 input_list = input_data.split(' ')
 result_list = []
-for i in range(len(input_list) // 2):
-    result_list.append(input_list[1::2][i])
-    result_list.append(input_list[::2][i])
+for i, j in zip(input_list[1::2], input_list[::2]):
+    result_list.append(i)
+    result_list.append(j)
 if len(input_list) % 2 != 0:
     result_list.append(input_list[-1])
 print(result_list)
 
+# for i in range(len(input_list) // 2):
+#     result_list.append(input_list[1::2][i])
+#     result_list.append(input_list[::2][i])
+# if len(input_list) % 2 != 0:
+#     result_list.append(input_list[-1])
+# print(result_list)
